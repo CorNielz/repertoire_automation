fd67d1b77904ff247253a8aa4ef33b9a20b3ab13
Daniel Vieira <danielhenrique.vieira@protonmail.com>
2025-04-26 15:25:25 -0300

    keyboard Open/Closed and refinement on Single Responsability

--- Models/keyboard.py
+++ Models/keyboard.py
@@ -10,43 +10,63 @@ class KeyboardActionHandler:
     def press(self, keyboard_key: str) -> None:        
         pyautogui.press(keyboard_key, interval=KEY_PRESS_INTERVAL)
 
-    def hold(self, keyboard_key: str):
+    def hold(self, keyboard_key: str) -> None:
         pyautogui.keyDown(keyboard_key)
         self.keys_held.append(keyboard_key)
 
-    def release(self, keyboard_key):
+    def release(self, keyboard_key: str) -> None:
         pyautogui.keyUp(keyboard_key)
         self.keys_held.remove(keyboard_key)
 
+    def is_held(self, keyboard_key: str) -> bool:
+        return keyboard_key in self.keys_held
+    
     def __init__(self):
         self.keys_held = []
 
 
 class KeyboardManager:
-    def run(self):
+    def run(self) -> None:
         while True:
             if self.queue.empty():
                 continue
         
             request = self.queue.get()
 
+            if not request:
+                continue
+
             keyboard_key = request.get("key")
             action = request.get("action")
 
-            if action == KeyboardAction.PRESS:
-                self.press(keyboard_key)
-            elif action == KeyboardAction.HOLD and not keyboard_key in self.keys_held:
-                self.hold(keyboard_key)
-            elif action == KeyboardAction.HOLD and keyboard_key in self.keys_held:
-                self.release(keyboard_key)
+            handler = self.action_map.get(action)
 
+            if not handler:
+                continue
+
+            handler(keyboard_key)
+
+    def _handle_press(self, keyboard_key: str) -> None:
+        self.action_handler.press(keyboard_key)
+
+    def _handle_hold(self, keyboard_key: str) -> None:
+        if not self.action_handler.is_held(keyboard_key):
+            self.action_handler.hold(keyboard_key)
+        else:
+            self.action_handler.release(keyboard_key)
+        
     def __init__(self, queue: Queue, action_handler: KeyboardActionHandler):
         self.queue = queue
         self.action_handler = action_handler
 
-    
+        self.action_map = {
+            KeyboardAction.PRESS: self._handle_press,
+            KeyboardAction.HOLD: self._handle_hold,
+        }
+        
 
-def start_keyboard(queue: Queue):
+
+def start_keyboard(queue: Queue) -> None:
     action_handler = KeyboardActionHandler()
     keyboard_manager = KeyboardManager(queue, action_handler)
 
