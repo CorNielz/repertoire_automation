8ca1119499d82b019530efa72b83569a98b439f1
Daniel Vieira <danielhenrique.vieira@protonmail.com>
2025-04-26 17:42:51 -0300

    Main adaptation for SOLID

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from Models.game_interface import GameInterface, GameInterfaceDetection
 from Models.keyboard import start_keyboard
+from Models.work import ProcessesManager
 
-from repertoire_automation import RepertoireAutomation
+from repertoire_automation import RepertoireAutomation, InterfaceManager, AutoplayManager, KeyProcessor, NoteFetcher, WorkManager
 
 from multiprocessing import Queue
 from multiprocessing import Process
@@ -12,4 +14,16 @@ if __name__ == "__main__":
     keyboard_process.daemon = True
     keyboard_process.start()
 
-    RepertoireAutomation.run(keyboard_queue)
+    game_interface_detection = GameInterfaceDetection()
+    interface_manager = InterfaceManager(game_interface_detection)
+
+    game_interface = GameInterface()
+    note_fetcher = NoteFetcher()
+    key_processor = KeyProcessor(note_fetcher)
+    processes_manager = ProcessesManager()
+    work_manager = WorkManager(processes_manager)
+    autoplay_manager = AutoplayManager(game_interface, key_processor, work_manager)
+
+    repertoire_automation = RepertoireAutomation(interface_manager, autoplay_manager)
+
+    repertoire_automation.run(keyboard_queue)
